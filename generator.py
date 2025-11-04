c7f0b34fd9251643ca53cfc1d36ab080cf82146f
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-11-04 12:08:32 +0800

    integer coeff

--- generator.py
+++ generator.py
@@ -171,19 +171,17 @@ def expanded_three_square_terms(x_unk: str = "x", y_unk: str = "y", num_unk: str
     number = random_coeff()
     number_square = number**2
 
-    number_square_str = str(number_square)
-
     if num_unk != "":
-        number_square_str = append_unk_to_coeff(num_unk, number_square_str, 2)
+        number_square_term = append_unk_to_coeff(num_unk, number_square, 2)
 
     expanded_polynomial = []
 
     if perfect_square_common_factor_coeff < 0:
-        append_all_to_list(expanded_polynomial, f"+{number_square_str}", perfect_square)
+        append_all_to_list(expanded_polynomial, f"+{number_square_term}", perfect_square)
 
         answer = f"({number}+)"
     else:
-        append_all_to_list(expanded_polynomial, perfect_square, f"-{number_square_str}")
+        append_all_to_list(expanded_polynomial, perfect_square, f"-{number_square_term}")
 
     return(expanded_polynomial)
 
