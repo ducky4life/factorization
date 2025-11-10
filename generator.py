8d1bd4c9fecd9d477df4c7081b7d19d70b6b341c
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-11-10 09:48:49 +0000

    index

--- generator.py
+++ generator.py
@@ -63,7 +63,7 @@ def flip_linear_term(term: str):
     local_superscript_list = superscript_list
     term = term.removeprefix("(")
     term_power = term.split(")")[-1]
-    term_list = term.split("-")
+    term_list = term[0:term.index(")")].split("-")
 
     new_term = "(" + term_list[-1] + "-" + term_list[0] + ")"
     if term_power in local_superscript_list:
@@ -72,7 +72,7 @@ def flip_linear_term(term: str):
 
 def multiply_term_to_polynomial(term: str, polynomial_list: list, random_flip_sign: bool = False, higher_degree: bool = False):
     local_superscript_list = superscript_list
-    linear_term = term[0:term.index(")")]
+    linear_term = term[0:term.index(")")+1]
     added_term = term
     new_list = []
 
