afc3e98000bb4ca8d8e87924bab81601a76a115d
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-11-10 09:56:39 +0800

    linear power

--- generator.py
+++ generator.py
@@ -72,8 +72,8 @@ def flip_linear_term(term: str):
 
 def multiply_term_to_polynomial(term: str, polynomial_list: list, random_flip_sign: bool = False, higher_degree: bool = False):
     local_superscript_list = superscript_list
-    linear_term = term.split(")")[-1]
-    linear_term = linear_term if linear_term in local_superscript_list else ""
+    linear_power = term.split(")")[-1]
+    linear_term = term[0:-2] if linear_power in local_superscript_list else term
     added_term = term
     new_list = []
     
@@ -83,7 +83,7 @@ def multiply_term_to_polynomial(term: str, polynomial_list: list, random_flip_si
             if is_flip_sign:
                 added_term = flip_linear_term(term)
                 
-        if term in item or flip_linear_term(term) in item or linear_term in item:
+        if linear_term in item or flip_linear_term(linear_term) in item:
             if higher_degree:
                 item_power = local_superscript_list.index(term.split(")")[-1]) + 1
                 print("item power ",item_power)
